eed887a74bd9c8a812eb78db730445c7c3aac640
Carlos Noleto <emanoelsjp14@gmail.com>
2025-07-28 21:03:26 -0300

    atulizando

--- recipes/views.py
+++ recipes/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html',
+                  context={
+                      'name': 'Carlos',
+                      'age': 25,
+                      'is_published': True,
+                  })
 
+def recipe(request):
+    return render(request, 'recipes/pages/home.html',
+                  context={
+                      'name': 'Carlos',
+                      'age': 25,
+                      'is_published': True,
+                  })
